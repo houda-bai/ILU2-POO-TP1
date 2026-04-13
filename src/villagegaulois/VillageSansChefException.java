fbe11919f57805d1c2b2806cd95e58a15efea462
houda-bai <houda.baidane@univ-tlse3.fr>
2026-04-13 08:23:16 +0200

    fini

--- src/villagegaulois/VillageSansChefException.java
+++ src/villagegaulois/VillageSansChefException.java
@@ -1,7 +1,7 @@
 package villagegaulois;
 
 public class VillageSansChefException extends Exception {
-
+	private static final long serialVersionUID =1L; 
     public VillageSansChefException() {
         super("Le village n'a pas de chef !");
     }

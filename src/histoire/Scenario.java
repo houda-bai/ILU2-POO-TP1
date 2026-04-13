6edb3d13415da09bcdc6d3fea7e04353f17c4904
houda-bai <houda.baidane@univ-tlse3.fr>
2026-04-13 08:19:32 +0200

    fin tp1

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -9,54 +9,59 @@ import villagegaulois.VillageSansChefException;
 
 public class Scenario {
 
-    public static void main(String[] args) {
-        // Création du village
-        Village village = new Village("le village des irréductibles", 10, 5);
+	public static void main(String[] args) {
+		// Création du village
+		Village village = new Village("le village des irréductibles", 10, 5);
 
-        // Création du chef
-        Chef abraracourcix = new Chef("Abraracourcix", 10, village);
-        village.setChef(abraracourcix);
+		// Création du chef
+		Chef abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
 
-        // Création des habitants
-        Druide druide = new Druide("Panoramix", 2, 5, 10);
-        Gaulois obelix = new Gaulois("Obélix", 25);
-        Gaulois asterix = new Gaulois("Astérix", 8);
-        Gaulois assurancetourix = new Gaulois("Assurancetourix", 2);
-        Gaulois bonemine = new Gaulois("Bonemine", 7);
+		// Création des habitants
+		Druide druide = new Druide("Panoramix", 2, 5, 10);
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		Gaulois assurancetourix = new Gaulois("Assurancetourix", 2);
+		Gaulois bonemine = new Gaulois("Bonemine", 7);
 
-        // on ajoute les habitants au village
-        village.ajouterHabitant(bonemine);
-        village.ajouterHabitant(assurancetourix);
-        village.ajouterHabitant(asterix);
-        village.ajouterHabitant(obelix);
-        village.ajouterHabitant(druide);
-        village.ajouterHabitant(abraracourcix);
+		// on ajoute les habitants au village
+		village.ajouterHabitant(bonemine);
+		village.ajouterHabitant(assurancetourix);
+		village.ajouterHabitant(asterix);
+		village.ajouterHabitant(obelix);
+		village.ajouterHabitant(druide);
+		village.ajouterHabitant(abraracourcix);
 
-        // affichge les villageois
-        try {
-            System.out.println(village.afficherVillageois());
-        } catch (VillageSansChefException e) {
-            System.out.println(e.getMessage());
-        }
+		// affichge les villageois
+		try {
+			System.out.println(village.afficherVillageois());
+		} catch (VillageSansChefException e) {
+			System.out.println(e.getMessage());
+		}
 
-        // Installation des vendeurs
-        System.out.println(village.installerVendeur(bonemine, "fleurs", 20));
-        System.out.println(village.installerVendeur(assurancetourix, "lyres", 5));
-        System.out.println(village.installerVendeur(obelix, "menhirs", 2));
-        System.out.println(village.installerVendeur(druide, "fleurs", 10));
+		// Installation des vendeurs
+		System.out.println(village.installerVendeur(bonemine, "fleurs", 20));
+		System.out.println(village.installerVendeur(assurancetourix, "lyres", 5));
+		System.out.println(village.installerVendeur(obelix, "menhirs", 2));
+		System.out.println(village.installerVendeur(druide, "fleurs", 10));
 
-        // Acheter des produits
-        Etal etalFleur = village.rechercherEtal(bonemine);
-        if (etalFleur != null) {
-            System.out.println(etalFleur.acheterProduit(10, abraracourcix));
-            System.out.println(etalFleur.acheterProduit(15, obelix));
-            System.out.println(etalFleur.acheterProduit(15, assurancetourix));
-        }
+		// Acheter des produits
+		Etal etalFleur = village.rechercherEtal(bonemine);
+		// recuperer l'execption de acheter produit 
+		try {
+		System.out.println(etalFleur.acheterProduit(10, abraracourcix));
+		System.out.println(etalFleur.acheterProduit(15, obelix));
+		System.out.println(etalFleur.acheterProduit(15, assurancetourix));
+		}
+		catch(IllegalStateException e) {
+			e.printStackTrace();
+			System.out.println("l'etal n'est pas occupe ");
+		}
+		
+		// Libérer les étals
+		System.out.println(village.partirVendeur(bonemine));
 
-        // Libérer les étals
-        System.out.println(village.partirVendeur(bonemine));
-
-        // Afficher le marché après libération
-        System.out.println(village.afficherMarche());
-    }
+		// Afficher le marché après libération
+		System.out.println(village.afficherMarche());
+	}
 }
--- src/villagegaulois/VillageSansChefException.java
+++ src/villagegaulois/VillageSansChefException.java
@@ -5,4 +5,15 @@ public class VillageSansChefException extends Exception {
     public VillageSansChefException() {
         super("Le village n'a pas de chef !");
     }
+    public VillageSansChefException(String message) {
+        super(message);
+    }
+
+    public VillageSansChefException(Throwable cause) {
+        super(cause);
+    }
+
+    public VillageSansChefException(String message, Throwable cause) {
+        super(message,cause);
+    }
 }
